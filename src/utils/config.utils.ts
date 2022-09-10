ad94a213d95be27b391035b848f3c0d188f9c2de
Peter Akaliro <pin4eva@hotmail.com>
2022-09-10 17:11:11 +0100

    add cloudinary package

--- src/utils/config.utils.ts
+++ src/utils/config.utils.ts
@@ -5,4 +5,7 @@ dotenv.config();
 export const config = {
   SECRET: process.env.SECRET || "jdkjdjkdk",
   MONGO_URI: process.env.MONGO_URI || "mongodb://localhost/jh",
+  CLOUDINARY_NAME: process.env.CLOUDINARY_NAME,
+  CLOUDINARY_KEY: process.env.CLOUDINARY_KEY,
+  CLOUDINARY_SECRET: process.env.CLOUDINARY_SECRET,
 };
